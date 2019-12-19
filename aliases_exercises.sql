86c5658aa9ac20237e3a9ebee62b4ad7e85f684f
Joshua Peters <peters.l.joshua@gmail.com>
2019-12-19 14:29:57 -0600

    afternoon save for exercises

---
+++ aliases_exercises.sql
@@ -0,0 +1,10 @@
+USE employees;
+
+-- Return 10 employees in a result set named 'full_name' in the
+-- format of 'last name, first name' for each employee.
+
+SELECT CONCAT(emp_no, ' - ',last_name ,', ' ,first_name) AS full_name, birth_date AS DOB
+FROM employees
+GROUP BY emp_no, last_name, first_name, birth_date
+LIMIT 10;
+
